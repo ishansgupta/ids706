27e98dace9eff0b5d63ba2cd4234490a6c6b30c9
Ishan Gupta <tv.bug.ct@gmail.com>
2020-09-08 01:22:53 -0400

    Add sample submission

---
+++ assignments/sample_submission.sql
@@ -0,0 +1,20 @@
+--1. Find count of rows in flights 
+SELECT COUNT(*) FROM flights;
+/* Answer goes here*/
+/*
+SOME_RANDOM_NUMBER
+ */
+
+--2. Find the top 5 flights for something
+SELECT some_random_select FROM some_random_table LIMIT
+
+/*
+ fl_num
+--------
+ 3430
+ 3669
+ 3484
+ 3432
+ 3431
+
+ */
